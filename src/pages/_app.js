39702e39f29e4059cd4db7469af2cdeb4246cbc8
ruscalworld <me@ruscal.world>
2023-04-25 21:23:15 +0300

    Add vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,7 @@ import en from '@/i18n/en.json'
 import { Navigation } from '@/components/Navigation'
 import { Exo_2, Sofia_Sans } from 'next/font/google'
 import { FontContext } from '@/components/FontProvider'
+import { Analytics } from '@vercel/analytics/react'
 
 const exo_2 = Exo_2({ subsets: [ 'cyrillic', 'latin' ] })
 const sofia_sans_semi_condensed = Sofia_Sans({ subsets: [ 'cyrillic', 'latin' ] })
@@ -28,11 +29,14 @@ export default function App({ Component, pageProps }) {
 
     // noinspection HtmlUnknownAttribute
     return (
-        <Navigation>
-            <style jsx global>{`html { font-family: ${ exo_2.style.fontFamily } `}</style>
-            <FontContext.Provider value={{ default: exo_2.className, highlight: sofia_sans_semi_condensed.className }}>
-                <Component { ...pageProps } />
-            </FontContext.Provider>
-        </Navigation>
+        <>
+            <Analytics/>
+            <Navigation>
+                <style jsx global>{`html { font-family: ${ exo_2.style.fontFamily } `}</style>
+                <FontContext.Provider value={{ default: exo_2.className, highlight: sofia_sans_semi_condensed.className }}>
+                    <Component { ...pageProps } />
+                </FontContext.Provider>
+            </Navigation>
+        </>
     )
 }
